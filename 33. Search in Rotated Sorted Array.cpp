8592dcc02e447e9b31ad398403b98a5009d69955
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-11-20 00:07:10 +0530

    33. Search in Rotated Sorted Array

--- 33. Search in Rotated Sorted Array.cpp
+++ 33. Search in Rotated Sorted Array.cpp
@@ -11,7 +11,7 @@ public:
 
         while (left <= right)
         {
-            mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target)
                 return mid;
 
