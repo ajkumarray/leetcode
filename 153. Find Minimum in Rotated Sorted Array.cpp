05c6e4fe39962a04ab666921f76a661fbc381fe8
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-11-20 16:50:27 +0530

    153. Find Minimum in Rotated Sorted Array

---
+++ 153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,30 @@
+//  153. Find Minimum in Rotated Sorted Array
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution //   find minimum in rotated sorted array
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        int left = 0, right = nums.size() - 1;
+        int ans = -1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= nums[0])
+                left = mid + 1;
+            else
+            {
+                ans = nums[mid];
+                right = mid - 1;
+            }
+        }
+        if (ans == -1)
+            return nums[0];
+
+        return ans;
+    }
+};
