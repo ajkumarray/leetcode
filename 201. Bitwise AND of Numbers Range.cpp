8ef67dd44a6fad870c1565ba6fce00ed2fbdee0c
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-12-07 23:31:26 +0530

    201. Bitwise AND of Numbers Range

---
+++ 201. Bitwise AND of Numbers Range.cpp
@@ -0,0 +1,24 @@
+//  201. Bitwise AND of Numbers Range
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int rangeBitwiseAnd(int left, int right)
+    {
+        if ((left == 0) || (right == 0))
+            return 0;
+
+        if ((int)log2(left) != (int)log2(right))
+            return 0;
+
+        int ans = left;
+        for (long i = left; i <= right; i++)
+            ans &= i;
+
+        return ans;
+    }
+};
