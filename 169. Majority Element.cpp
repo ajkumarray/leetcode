ca84818f62320e00c68836ef958138f738e3113d
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2022-01-18 10:29:57 +0530

    169. Majority Element

---
+++ 169. Majority Element.cpp
@@ -0,0 +1,20 @@
+//  169. Majority Element
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int count = 0, candidate = 0;
+        for (int num : nums)
+        {
+            if (count == 0)
+                candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+};
